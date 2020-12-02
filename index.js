351f54f4e16feb7b3b7f333c9bae5108cbcc8290
dodadoa <maddodadoa@gmail.com>
2020-12-02 22:24:56 +0700

    add press key arrow left, add safety net for selector

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-const { openBrowser, goto, $, dragAndDrop, closeBrowser, click, mouseAction, waitFor } = require('taiko');
+const { openBrowser, goto, $, dragAndDrop, closeBrowser, click, press, waitFor } = require('taiko');
 
 (async () => {
   try {
-    await openBrowser()
-    await goto("https://www.google.co.th/maps/@18.8845604,100.1593356,8z")
+    await openBrowser({ headless: false })
+    await goto("https://www.google.co.th/maps/@18.7905292,98.9863039,17.97z")
+    let enterStreetView = false;
     while(true) {
-      const up = Math.floor(Math.random() * 1000) + 100
-      const down = Math.floor(Math.random() * 1000)
-      const left = Math.floor(Math.random() * 1000) + 500
-      const right = Math.floor(Math.random() * 1000)
 
-      if ($(".widget-expand-button-pegman-icon").exists()){
-        await dragAndDrop($(".widget-expand-button-pegman-icon"), {up, left})
-        await waitFor(2000)
-
-        for(let i = 0; i < 5; i++){
-          const x = Math.floor(Math.random() * 1000)
-          const y = Math.floor(Math.random() * 1000)
-          await click({x,y})
-          await waitFor(1000)
-  
-          await mouseAction('press', { x: 800, y: 500 })
-          await mouseAction('move', { x: 200, y: 500 })
-          await mouseAction('release', { x: 200, y: 500 })
-        }
-
-        await waitFor(500)
+      while(!enterStreetView) {
+        const up = Math.floor(Math.random() * 1000) + 100
+        // const down = Math.floor(Math.random() * 1000)
+        const left = Math.floor(Math.random() * 1000) + 500
+        // const right = Math.floor(Math.random() * 1000)
+        await dragAndDrop($(".widget-expand-button-pegman-icon"), { up, left })
+        await waitFor(1000)
+        enterStreetView = await $(".widget-titlecard-exitcontainer").exists()
       }
 
-      if ($(".widget-titlecard-exitcontainer").exists()){
-        await click($(".widget-titlecard-exitcontainer"))
+      for(let i = 0; i < 5; i++){
+        const x = Math.floor(Math.random() * 1000) + 300
+        const y = Math.floor(Math.random() * 1000) + 100
+        await click({x,y})
+        await waitFor(1000)
+        await press(['ArrowLeft'], { delay: 1000 })
       }
+
+      await waitFor(500)
+
+      enterStreetView = false
+      await click($(".widget-titlecard-exitcontainer"))
     }
   } catch (error) {
       console.error(error);
