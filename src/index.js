47ecddf53a9566578079714667459690e8861db4
dodadoa <maddodadoa@gmail.com>
2020-12-03 01:27:37 +0700

    modify the random and take picture only first time and last time on google street view to create more disparity of photo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { openBrowser, goto, $, dragAndDrop, closeBrowser, click, press, waitFor } from 'taiko'
+import chalk from 'chalk'
 
 (async () => {
   try {
@@ -14,19 +15,29 @@ import { openBrowser, goto, $, dragAndDrop, closeBrowser, click, press, waitFor 
         enterStreetView = await $(".widget-titlecard-exitcontainer").exists()
       }
 
+      const url = await currentURL()
+      const streetViewLocation = url.split("/")[4]
+      const latLong = streetViewLocation.split(",").slice(0, 2)
+      console.log(chalk.yellow(` ✔ we are at ${latLong}`))
+      await screenshot({ fullPage:true, path: `images/${Date()}.png` })
+      await waitFor(100)
+
       for(let i = 0; i < 5; i++){
-        const x = Math.floor(Math.random() * 800) + 300
-        const y = Math.floor(Math.random() * 500) + 100
+        if(i === 4) {
+          await screenshot({ fullPage:true, path: `images/${Date()}.png` })
+        }
+
         const shouldBotTurnRandomNumber = Math.random()
-        await click({ x, y })
-        await waitFor(100)
-        if (shouldBotTurnRandomNumber < 0.3) {
+
+        if (shouldBotTurnRandomNumber < 0.2) {
           await press(['ArrowLeft'], { delay: 1000 })
-        } else if (shouldBotTurnRandomNumber > 0.3 && shouldBotTurnRandomNumber < 0.6) {
+        } else if (shouldBotTurnRandomNumber > 0.2 && shouldBotTurnRandomNumber < 0.4) {
           await press(['ArrowRight'], { delay: 1000 })
         } else {
-          // do nothing
-        } 
+          const x = Math.floor(Math.random() * 800) + 300
+          const y = Math.floor(Math.random() * 500) + 100
+          await click({ x, y })
+        }
       }
 
       await waitFor(100)
